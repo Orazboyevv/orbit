c8356ca84d1bfb1cc26edc068ad21a03bf168f23
G'iyosbek <161326034+Orazboyevv@users.noreply.github.com>
2024-05-17 17:28:30 +0500

    Initial commit

---
+++ orbit/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import About, Category, Portfolio, Server, Resume, Cleant, Contact
+
+# Register your models here.
+
+
+admin.site.register(About)
+admin.site.register(Category)
+admin.site.register(Portfolio)
+admin.site.register(Server)
+admin.site.register(Resume)
+admin.site.register(Cleant)
+admin.site.register(Contact)
---
+++ orbit/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import About, Portfolio, Category, Server, Resume, Cleant
+from .forms import ContactForm
+# Create your views here.
+
+
+def index(request):
+    about = About.objects.all()
+    porfolio = Portfolio.objects.all()
+    category = Category.objects.all()
+    server = Server.objects.all()
+    resume = Resume.objects.all()
+    cleant = Cleant.objects.all()
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('.')
+    ctx = {
+        "about": about,
+        "portfolio": porfolio,
+        "category": category,
+        "server": server,
+        "resume": resume,
+        "cleant": cleant,
+        'form': form
+    }
+    return render(request, 'index.html', ctx)
